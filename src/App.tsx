cae9b43c8beacb32857f87eec645251ebeccb2ff
Pedro Carvalho <pedrocamposcarvalho97@gmail.com>
2023-01-04 21:14:53 -0300

    remove routes folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import './config/ReactotronConfig';
-import { StatusBar, View } from 'react-native';
+import { LogBox, StatusBar, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { RFValue } from 'react-native-responsive-fontsize';
 import Home from './pages/Home';
 import colors from './utils/colors';
 import store from './store';
 
-const App: React.FC = () => (
-  <View
-    style={{
-      flex: 1,
-      backgroundColor: colors.grey,
-      paddingTop: RFValue(20),
-    }}
-  >
-    <StatusBar barStyle="dark-content" />
-    <Provider store={store}>
-      <Home />
-    </Provider>
-  </View>
-);
+const App: React.FC = () => {
+  LogBox.ignoreAllLogs();
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.grey,
+        paddingTop: RFValue(20),
+      }}
+    >
+      <StatusBar barStyle="dark-content" />
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    </View>
+  );
+};
 
 export default App;
